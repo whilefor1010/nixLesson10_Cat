302f8b995356333b375a18b6a726d4f43a299abd
whilefor1010 <whilefor1010@gmail.com>
2022-11-17 20:28:19 +0200

    add amazing cats

--- src/main/java/org/example/DayOfWeek.java
+++ src/main/java/org/example/DayOfWeek.java
@@ -1,2 +1,5 @@
-package org.example;public enum DayOfWeek {
+package org.example;
+
+public enum DayOfWeek {
+    MON, TUE, WED, THU, FRI, SAT, SUN, ALWAYS
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,9 +9,61 @@ import org.junit.Test;
 public class AppTest 
 {
 
+    final Cat Kuzja = new Cat("grrrey", 4.3, "amazing");
+    final Cat Fanja = new Cat("persik", 8.4, "vellikolepniy");
+
+    final Cat Etalon = new Cat("persik", 8.4, "vellikolepniy");
+
+
+    private String eSleep = Etalon.sleep();
+    private String eHunt = Etalon.hunt();
+    private String eRun = Etalon.run();
+    private String eCensored = Etalon.censored();
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldSleep()
     {
-        assertTrue( true );
+
+        assertEquals(eSleep, Fanja.whatDoesCatDo(DayOfWeek.MON));
+        assertEquals(eSleep, Fanja.whatDoesCatDo(DayOfWeek.WED));
+        assertEquals(eSleep, Kuzja.whatDoesCatDo(DayOfWeek.FRI));
+
     }
+
+    @Test
+    public void shouldHunt()
+    {
+
+        assertEquals(eHunt, Kuzja.whatDoesCatDo(DayOfWeek.TUE));
+        assertEquals(eHunt, Fanja.whatDoesCatDo(DayOfWeek.THU));
+        assertEquals(eHunt, Kuzja.whatDoesCatDo(DayOfWeek.SAT));
+
+    }
+
+    @Test
+    public void shouldRun()
+    {
+
+        assertEquals(eRun, Kuzja.whatDoesCatDo(DayOfWeek.SUN));
+        assertEquals(eRun, Fanja.whatDoesCatDo(DayOfWeek.SUN));
+
+    }
+
+    @Test
+    public void shouldEquals()
+    {
+
+        assertTrue(Fanja.equals(Etalon));
+        assertTrue(Etalon.equals(Fanja));
+
+    }
+
+    @Test
+    public void shouldCensored()
+    {
+
+        assertEquals(eCensored, Fanja.whatDoesCatDo(DayOfWeek.ALWAYS));
+
+    }
+
 }

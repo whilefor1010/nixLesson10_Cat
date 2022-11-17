53cdae9197cdd48204abd0f06b1ee04e5c035772
whilefor1010 <whilefor1010@gmail.com>
2022-11-17 20:27:26 +0200

    add amazing cats

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
